bf8715ac4b95d542afffb5cbb068b72b3cde7cd9
Jake Crawford <jake.crawford@mtsi-va.com>
2018-08-22 17:17:59 -0400

    Auto-script commit

--- python/twitterTests.py
+++ python/twitterTests.py
@@ -1,8 +1,10 @@
 import unittest
+import redis
 import location
 
 with open("/var/local/code/sampleJSON/sampleIn.json", "r") as json:
    json_text = json.read()
+red = redis.Redis(host='localhost', port=6379, password='')
 
 class TestTwitter(unittest.TestCase):
 
@@ -31,3 +33,8 @@ class TestTwitter(unittest.TestCase):
       self.assertEqual(location.swapCoordinates("[1.1, 2.2]"), "[2.2, 1.1]")
       self.assertEqual(location.swapCoordinates(None), None)
       self.assertEqual(location.swapCoordinates("None"), None)
+
+   #All redis keys should be lowercase
+   def testRedisHasKey(self):
+      self.assertTrue(location.redisHasKey(red, "va"))
+      self.assertFalse(location.redisHasKey(red, "VA"))
