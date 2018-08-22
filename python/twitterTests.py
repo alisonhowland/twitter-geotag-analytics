b59bb240b08c5b32303d17af0308791b329f8388
Jake Crawford <jake.crawford@mtsi-va.com>
2018-08-22 12:36:13 -0400

    Auto-script commit

---
+++ python/twitterTests.py
@@ -0,0 +1,25 @@
+import unittest
+import location
+
+with open("/var/local/code/sampleJSON/sampleIn.json", "r") as json:
+   json_text = json.read()
+
+class TestTwitter(unittest.TestCase):
+
+   def testHasTweet(self):
+      self.assertTrue(location.hasTweet(json_text))
+
+   def testGetTweet(self):
+      self.assertEqual(location.getTweet(json_text), "@nikki_nola_ @KPeynado22 Idk why people have a hard time understanding its savory and nothing else. They can get cl… https://t.co/Fa1gpDu27S")
+
+   def testGetLanguage(self):
+      self.assertEqual(location.getLanguage(json_text), "en")
+
+   def testGetLocation(self):
+      self.assertEqual(location.getLocation(json_text), "Philly ➡️ D[M]V")
+
+
+   def testSetLocation(self):
+      loc = "anywhere"
+      new_text = location.setLocation(loc, json_text)
+      self.assertEqual(loc, location.getLocation(new_text))
