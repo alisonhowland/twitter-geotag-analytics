c284229c80df093d8d27c3a2dfe697dcc15276dc
Jake Crawford <jake.crawford@mtsi-va.com>
2018-08-22 12:55:39 -0400

    Auto-script commit

--- python/twitterTests.py
+++ python/twitterTests.py
@@ -23,3 +23,11 @@ class TestTwitter(unittest.TestCase):
       loc = "anywhere"
       new_text = location.setLocation(loc, json_text)
       self.assertEqual(loc, location.getLocation(new_text))
+
+   def testHasLocation(self):
+      self.assertTrue(location.hasLocation(json_text))
+
+   def testSwapCoordinates(self):
+      self.assertEqual(location.swapCoordinates("[1.1, 2.2]"), "[2.2, 1.1]")
+      self.assertEqual(location.swapCoordinates(None), None)
+      self.assertEqual(location.swapCoordinates("None"), None)
